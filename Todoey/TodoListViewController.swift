e98c961734a7984d1bc2216c28fddc5e243a5494
Iurii Plugatarov <workstrange20@yahoo.com>
2018-06-17 19:26:28 +0300

    created table view renderer

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Iurii Plugatarov on 17/06/2018.
+//  Copyright © 2018 Iurii Plugatarov. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController : UITableViewController {
+  let itemArray = ["Find Someone", "Buy Food", "Eat burger"]
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  //MARK - Tableview Datasource Methods
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return itemArray.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+    cell.textLabel?.text = itemArray[indexPath.row]
+    
+    return cell
+  }
+}
+
